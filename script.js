c2f4abdf9b2c787bd85685ced9c0f7284b3cfb7b
alumno <alumno@Instituto.local>
2023-08-07 10:17:19 -0300

    cambios
    cambios

---
+++ script.js
@@ -0,0 +1,22 @@
+let suma = 3 + 3
+console.log("El resultado de la suma es: " + suma)
+
+let resta = 6 - 3
+console.log("El resultado de la resta es: " + resta)
+
+let multiplicacion = 3 * 3
+console.log("El resultado de la multiplicacion es : " + multiplicacion)
+
+let division = 3 / 3
+console.log("El resultado de la divicion es : " + division)
+
+alert("El resultado de la suma es: " + suma)
+alert("El resultado de la resta es: " + resta)
+alert("El resultado de la multiplicacion es: " + multiplicacion)
+alert("El resultado de la division es: " + division)
+
+let nombrealumno
+
+nombrealumno = prompt("Ingresa u nombre")
+
+alert("Bienvenido " + nombrealumno)
