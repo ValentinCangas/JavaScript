21be77e988d2e551b028903fd7ee4e2a1e77edcd
alumno <alumno@Instituto.local>
2023-08-07 10:37:43 -0300

    muchos cambios
    terminado

--- script.js
+++ script.js
@@ -15,8 +15,27 @@ alert("El resultado de la resta es: " + resta)
 alert("El resultado de la multiplicacion es: " + multiplicacion)
 alert("El resultado de la division es: " + division)
 
+let edadalumno = 2023 - 2008
+alert("Mi edad es " + edadalumno)
+
+let edad
+edad = prompt("Ingrsa tu año de nacimiento")
+
 let nombrealumno
 
 nombrealumno = prompt("Ingresa u nombre")
 
 alert("Bienvenido " + nombrealumno)
+
+let nombreAlumno = "Valentin"
+let apellidoAlumno = " Cangas"
+
+
+console.log("Bienvenido " + nombreAlumno + apellidoAlumno) 
+
+let diez = 10
+let tres = 3
+let resultadoMultiplicacion = diez * tres
+ 
+alert("El resultad de la multiplicacion es: " + resultadoMultiplicacion)
+console.log("El resultado de la multiplicacion es: " + resultadoMultiplicacion)
